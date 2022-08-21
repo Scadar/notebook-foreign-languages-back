cf388f3831b26e3f6394f6bcb2a0cff955730234
Roman Miroshnikov <roman.miroshnikov.work@yandex.ru>
2022-08-21 16:31:18 +0500

    Initial commit

---
+++ src/test/java/ru/notebookforeignlanguages/notebookforeignlanguagesback/NotebookForeignLanguagesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.notebookforeignlanguages.notebookforeignlanguagesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookForeignLanguagesBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
